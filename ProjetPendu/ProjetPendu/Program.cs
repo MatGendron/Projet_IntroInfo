c127287a6c14930da4051acc8b62db89419068fe
Mathis G <gendronmathis@gmail.com>
2020-11-24 10:30:30 +0100

    Premiers prototypes de fonction

--- ProjetPendu/ProjetPendu/Program.cs
+++ ProjetPendu/ProjetPendu/Program.cs
@@ -4,9 +4,56 @@ namespace ProjetPendu
 {
     class Program
     {
+        /* Cette fonction permet d'initialiser un jeu de pendu
+         */
+        static void initialiseJeu()
+        {
+
+        }
+        static void AfficheRegle()
+        {
+
+        }
+        /* Cette fonction permet d'afficher le pendu et le mot à compléter
+         */
+        static void AffichePendu()
+        {
+
+        }
+        /* Cette fonction permet à l'ordinateur de choisir un mot au début du jeu
+         */
+        static void ChoisitMotAleatoire()
+        {
+
+        }
+        /* Cette fonction permet à un joueur humain de choisir un mot au début du jeu
+         */
+        static void ChoisitMot()
+        {
+
+        }
+        /*Cette fonction permet à un joueur humain de jouer un tour
+         */
+        static void TourJoueur()
+        {
+
+        }
+        /* Cette fonction permet à un ordinateur de jouer un tour
+         */
+        static void TourOrdinateur()
+        {
+
+        }
+        /* Met fin eu jeu
+         */
+        static void AbandonJeu()
+        {
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Je ne veux pas travailler");
+
         }
     }
 }
+
