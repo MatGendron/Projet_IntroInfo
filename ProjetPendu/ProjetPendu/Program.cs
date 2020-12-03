693c891b286f6eb6ecb547f97ef532664775506e
Paul BERNARD <pbernard007@ensc.fr>
2020-12-03 12:24:56 +0100

    Main() + InitLexique

--- ProjetPendu/ProjetPendu/Program.cs
+++ ProjetPendu/ProjetPendu/Program.cs
@@ -1,9 +1,55 @@
 ﻿using System;
+using System.IO;
 
 namespace ProjetPendu
 {
     class Program
     {
+
+        enum FinDeTour
+        {
+            Continue,
+            Gagne,
+            Perdu,
+            Abandon
+        }
+        static void Main(string[] args)
+        {
+            bool compris;
+            string reponse;
+
+            string[] lexique = new string[323_572]; // c'est le nombre de mots dans le lexique
+            InitLexique(lexique);
+
+            AfficheRegle();
+
+            bool envie_de_jouer = true;
+			while(envie_de_jouer) {
+				Partie(lexique);
+
+				compris = false; reponse = "";
+
+				while(!compris) {
+					Console.WriteLine("Voulez-vous refaire une partie ? (oui/non)");
+					reponse = Console.ReadLine();
+					if (String.Equals(reponse,"oui")) {
+						envie_de_jouer=true;
+						compris=true;
+					}
+					else {
+						if (String.Equals(reponse,"non")) {
+							envie_de_jouer=false;
+							compris = true;
+						}
+						else {
+							Console.Write("Je n'ai pas compris. ");
+						}
+					}
+					
+				}
+			}
+
+        }
         /* Permet de demander à l'utilisateur quel type de partie il veut lancer
         */
         static void Partie(string[] lexique)
@@ -20,7 +66,10 @@ namespace ProjetPendu
         */
         static string ChoisirMotOrdi(string[] lexique)
         {
+            Random rnd = new Random();
+            int position = rnd.Next(lexique.Length);
 
+            return(lexique[position]);
         }
         /*Cette fonction permet à un joueur humain de jouer un tour
         */
@@ -36,19 +85,35 @@ namespace ProjetPendu
         }
         /* Cette fonction permet d'afficher le pendu et le mot à compléter
          */
-        static void AffichePendu()
+        static void AffichePendu(int degre)
         {
 
         }
-        /* Cette fonction permet à un ordinateur de jouer un tour
+        /* Cette fonction permet à un ordinateur de jouer un tour.
          */
         static void TourOrdinateur()
         {
 
         }
-        static void Main(string[] args)
+        /* Ouvre le fichier dicoFR.txt et insère les mots dans le tableau.
+        */
+        static void InitLexique(string[] lexique)
         {
+            System.Text.Encoding   encoding = System.Text.Encoding.GetEncoding(  "iso-8859-1"  );
+            StreamReader lecteur = new StreamReader("../dicoFR.txt",encoding);
 
+            string mot = lecteur.ReadLine();
+            int i = 0;
+
+            while (mot != null)
+            {
+                // On ne veut pas prendre les mots "A" et "Y", ni la dernière ligne vide.
+                if (mot.Length >= 2)
+                {
+                    lexique[i] = mot;
+                    mot = lecteur.ReadLine();
+                }
+            } 
         }
     }
 }
